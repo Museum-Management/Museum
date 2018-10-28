6d9b019022cb89418ece2aa8d3cc96be52060eaa
AhalyaR <43716765+AhalyaR@users.noreply.github.com>
2018-10-28 12:36:35 +0530

    Add files via upload

---
+++ loginconfirm.php
@@ -0,0 +1,38 @@
+<?php
+$servername = "localhost";
+$username = "museum";
+$password = "museum";
+$dbname = "museumdb";
+$mail=$_GET['email'];
+$psw=$_GET['password'];
+
+$conn=mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+} 
+
+
+$sql = "SELECT * FROM visitordet WHERE email='$mail'";
+
+$result=mysqli_query($conn,$sql);
+
+if(mysqli_num_rows($result)==0){
+	echo "email has not been registerd yet ";
+}
+
+$row=mysqli_fetch_assoc($result);
+
+ if(strcmp($psw,$row['id'])==0){
+	echo "successfully logged in ";
+}
+else{
+	echo "Incorrectpassword";
+	header('location: visitor login.php');
+}
+
+
+
+
+
+?>
